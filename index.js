7406ce442afb92c406da530d7b68da3ad39bea86
Matthew Young <myoung162744@gmail.com>
2018-05-12 19:40:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ function add(a,b){
   return n +1
 } function dec(n){
   return n -1
-} function makeInt(n){
-  return parseInt(n,10)
+} function makeInt(string){
+  return parseInt(string,10)
+} function preserveDecimal(n){
+  return parseFloat(n)
 }
